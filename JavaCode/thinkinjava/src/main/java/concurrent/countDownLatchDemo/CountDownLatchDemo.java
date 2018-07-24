47c4ce2703f70e3eb8e3514827fa19610c22d37c
Sq-List <574146616@qq.com>
2018-07-25 01:35:39 +0800

    在 并发.md 中增加 CAS、AQS、CountDownLatch

---
+++ JavaCode/thinkinjava/src/main/java/concurrent/countDownLatchDemo/CountDownLatchDemo.java
@@ -0,0 +1,34 @@
+package concurrent.countDownLatchDemo;
+
+import java.util.concurrent.CountDownLatch;
+
+public class CountDownLatchDemo {
+    public static void main(String[] args) {
+        final CountDownLatch countDownLatch = new CountDownLatch(2);
+
+        Runnable runnable = () -> {
+            try {
+                System.out.println("子线程" + Thread.currentThread().getName() + "正在执行");
+                Thread.sleep(1000);
+                System.out.println("子线程" + Thread.currentThread().getName() + "执行完毕");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                countDownLatch.countDown();
+            }
+        };
+
+        new Thread(runnable).start();
+
+        new Thread(runnable).start();
+
+        try {
+            System.out.println("等待两个子线程执行完毕");
+            countDownLatch.await();
+            System.out.println("两个子线程执行完毕");
+            System.out.println("执行主线程");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
